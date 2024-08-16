bde9f3ec318192cf1eb74ba7eff152701ed1e659
Dawid Pylak <dawpylak@gmail.com>
2024-08-16 17:47:26 +0200

    initial commit

---
+++ backend/src/test/java/com/amadon/patentconnector/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadon.patentconnector.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
